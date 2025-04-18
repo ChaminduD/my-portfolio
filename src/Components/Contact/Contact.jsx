8be52a239309c7c4f12444eb7f3caebf752b85aa
ChaminduD <chamindudulanjana10@gmail.com>
2025-04-18 17:13:24 +0530

    Fix reset form after submitting

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,7 +1,27 @@
+import { useEffect } from 'react'; 
 import './Contact.css';
 import PrimaryButton from '../PrimaryButton/PrimaryButton.jsx';
 
 function Contact(){
+    useEffect(() => {
+        const form = document.querySelector('.contact-form');
+
+        if (form) {
+            form.addEventListener('submit', (e) => {
+                e.preventDefault(); // Prevent navigation during testing
+                form.reset(); // Clears all inputs and textareas
+                window.location.href = 'https://formspree.io/thanks?language=en'; // Redirect to the thank-you page
+            });
+        }
+
+        // Cleanup the event listener when the component unmounts
+        return () => {
+            if (form) {
+                form.removeEventListener('submit', () => {});
+            }
+        };
+    }, []);
+
     return(
         <section id="contact" className="contact-container section">
             <h2 className="contact-title">Contact Me</h2>
